ba9e717b4c5120d7733e94dfe0efbd1b908a0b1c
Cristina Guerrero <cristina.guerrero@mercell.com>
2026-01-21 22:47:48 +0100

    Testing dagsordener

---
+++ dagsordener.aarhus.dk_dist_js_LocalizationUtil.b53b17d9a3a84e618ba6.js
@@ -0,0 +1,29 @@
+webpackJsonp([0,6],[
+/* 0 */
+/***/ (function(module, exports) {
+
+	function Localization() {
+	    this.localization = window.localizationKeys;
+	}
+	window.Localization = new Localization();
+	
+	/**
+	 * Returns the sum of a and b
+	 * @param {string} key
+	 * @returns {string}
+	 */
+	Localization.prototype.getText = function(key) {
+	    var value = this.localization.text[key];
+	
+	    if (value === undefined) {
+	        console.log("mangler key", key)
+	        return "BLANK";
+	    }
+	
+	    return value;
+	};
+
+
+/***/ })
+]);
+//# sourceMappingURL=LocalizationUtil.b53b17d9a3a84e618ba6.js.map
